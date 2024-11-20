39e9de63f0430a4eb2b05f9e39732ea81c7ca0a8
Chris Palmer <palmer@tabroom.com>
2024-11-20 07:37:11 -0500

    Upgraded tabulator.  Front page works again.  headers br mystery.

--- src/lib/DataTable.svelte
+++ src/lib/DataTable.svelte
@@ -1,14 +1,16 @@
 <script lang='ts'>
+
+	import {TabulatorFull as Tabulator} from 'tabulator-tables';
 	import {onMount} from 'svelte';
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	// export let data: any[];
+	export let data: any[];
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	// export let columns: any[] = [];
+	export let columns: any[] = [];
+
 	let tableComponent:HTMLDivElement;
 
 	onMount(() => {
-/**
 		new Tabulator(tableComponent, {
 			data,
 			columns,
@@ -19,20 +21,20 @@
 			pagination        : true,
 			paginationSize    : 50,
 		});
-**/
 	});
+
 </script>
-  
+
 	<div bind:this={tableComponent}></div>
-  
+
 	<svelte:head>
-<!--	<script 
-			type="text/javascript" 
+<!--	<script
+			type="text/javascript"
 			src="https://oss.sheetjs.com/sheetjs/xlsx.full.min.js"
 		></script>
 -->
 		<link
-			href="/css/tabulator.min.css" 
+			href="/css/tabulator.min.css"
 			rel="stylesheet"
 		>
 		<link href="/css/tabulator.css" rel="stylesheet">
