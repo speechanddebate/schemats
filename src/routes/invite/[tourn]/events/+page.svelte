003e3274b11e070090022a3ddfc1bdbdbed6ecba
Chris Palmer <palmer@tabroom.com>
2025-09-25 11:40:29 -0400

    some more details

--- src/routes/invite/[tourn]/events/+page.svelte
+++ src/routes/invite/[tourn]/events/+page.svelte
@@ -35,64 +35,66 @@
 
 	{#each pageContent.data?.events as event (event.id) }
 
-		<div class="flex w-full border-t-1 border-t-back-200">
+		<div class="flex w-full ms-2 mt-2">
 			<span class="flex grow">
 				<h6>{event.name}</h6>
 			</span>
 
-			<span class="flex text-right pr-2">
+			<span class="flex text-right pr-4">
 				<h6>{event.abbr}</h6>
 			</span>
 		</div>
 
-		<div class="px-2">
+		<div class="px-2 pb-4 pt-2
+			border-b-1 border-b-back-200"
+		>
 
-			{#if event.fee}
+			<div class="flex">
+				{#if event.fee}
+					<p class='flex grow'>
+						Entry Fee: ${event.fee}
+					</p>
+				{/if}
+
+				{#if event.schoolCap}
+					<p class='flex grow'>
+						Limited to {event.schoolCap} entries per school
+					</p>
+				{/if}
+
+				{#if event.cap}
+					<p class='flex grow'>
+						Limited to {event.cap} total entries
+					</p>
+				{/if}
+			</div>
+
+			{#if event.topicText}
+				<p><span class='font-semibold'>Topic:</span>{event.topicText || 'Resolved: this sucks'}</p>
+			{/if}
+
+			{#if event.topicTag}
 				<p>
-					Entry Fee: ${event.fee}
+					Topic:
+					{event.topicSource}
+					{event.topicEventType}
+					{event.topicTag}
 				</p>
 			{/if}
 
-		<div class="flex">
-			{#if event.schoolCap}
-				<p class='flex grow'>
-					Limited to {event.schoolCap} entries per school
-				</p>
+			{#if event.description}
+				<hr />
+				{@html event.description}
 			{/if}
 
-			{#if event.cap}
-				<p class='flex grow'>
-					Limited to {event.cap} total entries
-				</p>
+			{#if event.fieldReport}
+				<a
+					class = "text-primary-700 text-md ps-1"
+					href  = {resolve(`${page.url.pathname}/${event.id}/field`, {})}
+				>
+					Field Report
+				</a>
 			{/if}
-		</div>
-
-		{#if event.topicText}
-			<p><span class='font-semibold'>Topic:</span>{event.topicText || 'Resolved: this sucks'}</p>
-		{/if}
-
-		{#if event.topicTag}
-			<p>
-				Topic:
-				{event.topicSource}
-				{event.topicEventType}
-				{event.topicTag}
-			</p>
-		{/if}
-
-		{#if event.description}
-			<hr />
-			{@html event.description}
-		{/if}
-
-		{#if event.fieldReport}
-			<a
-				class = "underline"
-				href  = {resolve(`${page.url.pathname}/${event.id}/field`, {})}
-			>
-				Field Report
-			</a>
-		{/if}
 
 		</div>
 
