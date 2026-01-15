a8631958641277cc638b892cb5e3e45b59d0d250
Chris Palmer <palmer@tabroom.com>
2026-01-15 10:32:40 -0500

    tinker with it.  can't find the CSV yet

---
+++ src/lib/grid/SVGrid.svelte
@@ -0,0 +1,28 @@
+<script>
+
+	import { Grid } from '@svar-ui/svelte-grid';
+	import { Willow } from "@svar-ui/svelte-grid";
+
+	let { data, columns, options } = $props();
+
+	let grid;
+
+	function handleExport() {
+		if (grid) {
+			grid.exportToCSV({ filename: 'tabroom-export.csv' });
+		}
+	}
+</script>
+
+<button on:click={handleExport} class="border-2">
+  Export to CSV
+</button>
+
+ <Willow>
+	<Grid
+		bind:this={grid}
+		{data}
+		{columns}
+		{...options}
+	/>
+</Willow>
