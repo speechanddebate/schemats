7e41f908e207d43870ea73a73d4f7514d1b3a7a6
Chris Palmer <palmer@tabroom.com>
2026-01-15 15:17:07 -0500

    Mostly digging but found a few things

--- src/lib/grid/SVGrid.svelte
+++ src/lib/grid/SVGrid.svelte
@@ -1,28 +1,34 @@
 <script>
 
+    import { Pager } from '@svar-ui/svelte-core';
 	import { Grid } from '@svar-ui/svelte-grid';
-	import { Willow } from "@svar-ui/svelte-grid";
+	import { Willow } from '@svar-ui/svelte-grid';
 
 	let { data, columns, options } = $props();
 
-	let grid;
+	let limit = options.limit || import.meta.env.VITE_TOURN_LIMIT || 128;
 
-	function handleExport() {
-		if (grid) {
-			grid.exportToCSV({ filename: 'tabroom-export.csv' });
-		}
-	}
+	let pagedData = $state([]);
+
+	const setPage = (event) => {
+		const { from, to } = event;
+		pagedData = data.slice(from, to);
+	};
+
+	setPage({ from: 0, to: limit });
+
 </script>
-
-<button on:click={handleExport} class="border-2">
-  Export to CSV
-</button>
 
  <Willow>
 	<Grid
-		bind:this={grid}
-		{data}
 		{columns}
+		data = {pagedData}
 		{...options}
+	/>
+
+	<Pager
+		onchange = {setPage}
+		pageSize = {limit}
+		total    = {data.length}
 	/>
 </Willow>
