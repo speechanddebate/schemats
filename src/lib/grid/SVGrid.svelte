518b47b107448738c5e44b8d23d4df7df4176ae4
Chris Palmer <palmer@tabroom.com>
2026-01-16 14:08:59 -0500

    with hatred in my heart, CSV exports

--- src/lib/grid/SVGrid.svelte
+++ src/lib/grid/SVGrid.svelte
@@ -3,12 +3,13 @@
     import { Pager } from '@svar-ui/svelte-core';
 	import { Grid } from '@svar-ui/svelte-grid';
 	import { Willow } from '@svar-ui/svelte-grid';
+	import { Button } from 'flowbite-svelte';
+	import Papa from 'papaparse';
 
 	let { data, columns, options } = $props();
-
 	let limit = options.limit || import.meta.env.VITE_TOURN_LIMIT || 128;
-
 	let pagedData = $state([]);
+	let api = $state();
 
 	const setPage = (event) => {
 		const { from, to } = event;
@@ -17,18 +18,78 @@
 
 	setPage({ from: 0, to: limit });
 
+	const printPortrait = () => {
+		api.exec('print', {
+			paper: options.papersize || 'letter',
+			mode: 'paper',
+		});
+	};
+
+	const printLandscape = () => {
+		api.exec('print', {
+			paper: options.papersize || 'letter',
+			mode: 'landscape',
+		});
+	};
+
+	const csvGrid = async () => {
+		const csvData = Papa.unparse(data);
+		const blob = new Blob([csvData], {type: 'text/csv;charset=utf-8'});
+		const a       = document.createElement('a');
+		a.href        = URL.createObjectURL(blob);
+		a.download    = options.filename || 'tabroom-export.csv';
+		document.body.appendChild(a);
+		a.click();
+		document.body.removeChild(a);
+	};
+
 </script>
 
- <Willow>
-	<Grid
-		{columns}
-		data = {pagedData}
-		{...options}
-	/>
+<div class='p-4'>
 
-	<Pager
-		onchange = {setPage}
-		pageSize = {limit}
-		total    = {data.length}
-	/>
-</Willow>
+	<div class="flex">
+		<span class="w-1/3">
+			<h4>CSV Export grid</h4>
+		</span>
+
+		<span class="w-2/3 parent-toolbar text-right">
+			<Button
+				onclick={() => printPortrait()}
+			>Port</Button>
+
+			<Button
+				onclick={() => printLandscape()}
+			>Land</Button>
+
+			<Button
+				onclick={() => csvGrid()}
+			>CSV</Button>
+		</span>
+	</div>
+
+	<Willow>
+		<Grid
+			bind:this={api}
+			{columns}
+			data = {pagedData}
+			{...options}
+		/>
+
+		<Pager
+			onchange = {setPage}
+			pageSize = {limit}
+			total    = {data.length}
+		/>
+	</Willow>
+</div>
+
+<style>
+	.parent-toolbar :global(.tableToolbar) {
+		justify-content : right;
+		vertical-align  : center;
+	}
+
+	:global(.tableToolbar) {
+		display: inline;
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,3 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { sveltePreprocess } from 'svelte-preprocess';
 
@@ -9,10 +8,7 @@ const config = {
 	preprocess: [vitePreprocess(), sveltePreprocess()],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter(),
+		// adapter: adapter(), Not necessary for self deployment.
 	},
 
 	compilerOptions: {
