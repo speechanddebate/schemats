bc28819708f7a0d6ed3234b2911787916d7075ec
Ryan Tullis <tulliscryan@gmail.com>
2026-02-18 13:19:09 -0600

    npm updates and audit

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -5,8 +5,8 @@
   <div class="w-full text-center">
 	<h1>Error {page.status}</h1>
 	<p>{page.error?.message}</p>
-	
+
 	{#if page.status === 404}
-	  <p>This page could not be found.</p>
+		<p>This page could not be found.</p>
 	{/if}
   </div>
