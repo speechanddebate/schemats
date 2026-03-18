0d2e24277552d1ce760cac46c5e34034009f82f5
Ryan Tullis <tulliscryan@gmail.com>
2026-03-18 12:16:39 -0500

    add circuitsTable

--- src/routes/circuits/+page.svelte
+++ src/routes/circuits/+page.svelte
@@ -1,20 +1,13 @@
 <script lang="ts">
 import { createRestCircuitsActive } from '$indexcards';
 import { safeExtract } from '$lib/helpers/query';
+import CircuitsTable from '$lib/components/tables/circuitsTable.svelte';
 
 const circuitsQuery = createRestCircuitsActive();
 
 const circuitsData = $derived(safeExtract(circuitsQuery));
 
 </script>
-
-This is a page for displaying a list of active circuits
-Circuits: {circuitsData?.length}
-{#each circuitsData as circuit (circuit.id)}
-	<p><a href="circuits/{circuit.id}/calendar">id:{circuit.id}
-		abbr:{circuit.abbr}
-		name:{circuit.name}
-		state:{circuit.state}
-		country:{circuit.country}
-		tournCount:{circuit.tournCount}</a></p>
-{/each}
+<div>
+	<CircuitsTable {circuitsData}/>
+</div>
