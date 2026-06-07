7158a979616f7cae0f728c9325715add94f3236f
Chris Palmer <palmer@tabroom.com>
2026-06-07 01:31:06 -0400

    Revenge

--- config/config.sample.ts
+++ config/config.sample.ts
@@ -1,35 +1,35 @@
 import type { AppConfig } from './config.d';
 
 const config: AppConfig = {
-	ENV: 'development', // 'development' | 'production' | 'test'
-	WEB_URL: 'localhost', //the base URL for the web server, used for constructing links. Do not include protocol or port.
-	LEGACY_URL: 'https://mason.dev.tabroom.com',
-	S3_BASE: 'https://s3.amazonaws.com/tabroom-files',
+	ENV        : 'development', // 'development' | 'production' | 'test'
+	WEB_URL    : 'https://schemats.debatefail.com',
+	LEGACY_URL : 'https://mason.debatefail.com',
+	S3_BASE    : 'https://s3.amazonaws.com/tabroom-files',
 
 	vite: {
-		host: 'localhost',
-		port: 9000,
-		clientPort: 443, // The port on the other side of the NGINX proxy with SSL
-		previewPort: 9003,
+		host		: 'schemats.debatefail.com',
+		port		: 9000,
+		clientPort  : 443, // The port on the other side of the NGINX proxy with SSL
+		previewPort : 9003,
 	},
 
-	indexcards: {
-		host: 'http://localhost:8001', // Host for SSR
-		clientHost: 'http://localhost:9000', // Host for client-side (browser) access, proxied through Vite in development
-		basePath: '/v1',
-		authCookieName: 'Tabroom_Cookie',
-		csrfCookieName: 'CSRF_Token',
-		csrfTokenHeader: 'x-csrf-token',
-		sessionHeader: 'tabroom-sessionkey',
+	indexcards         : {
+		host           : 'https://api.debatefail.com',
+		clientHost     : 'https://api.debatefail.com',
+		basePath       : '/v1',
+		authCookieName : 'TabroomLocal',
+		csrfCookieName  : 'CSRF_Token',
+		csrfTokenHeader : 'x-csrf-token',
+		sessionHeader   : 'tabroom-sessionkey',
 	},
 
 	logging: {
 		level: 'info', // error, warn, info, debug
 		file: {
-			enabled: true,
-			path: 'logs',
-			maxSize: '20m',
-			maxFiles: 14,
+			enabled  : true,
+			path     : '/var/log/schemats/',
+			maxSize  : '512m',
+			maxFiles : 16,
 		},
 	},
 };
